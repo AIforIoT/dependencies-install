aaf13090b206230552e9119bd58831b086b98981
bieltv3 <bieltv.3@gmail.com>
2018-10-23 11:47:16 +0200

    automatic install

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 # Python 3
-apt-get install python3
-apt-get install python3-pip
+apt-get install python3 -y
+apt-get install python3-pip -y
 
 # Development dependencies
-apt-get install libatlas-base-dev
-apt-get install flac
-apt-get install git
+apt-get install libatlas-base-dev -y
+apt-get install flac -y
+apt-get install git -y
 
 # Local Speech recognition dependencies
-apt-get install libhdf5-serial-dev
-apt-get install swig
-apt-get install pulseaudio
-apt-get install libpulse-dev
-apt-get install libasound2-dev
+apt-get install libhdf5-serial-dev -y
+apt-get install swig -y
+apt-get install pulseaudio -y
+apt-get install libpulse-dev -y
+apt-get install libasound2-dev -y
 
 # Artificial inteligence package
 git clone https://github.com/aiforiot/artificial_inteligence.git
-pip3 install -r artificial_intelligence/requeriments.txt
+pip3 install -r artificial_intelligence/requeriments.txt  -y
 
 
